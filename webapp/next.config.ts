564ada6dc26df78a6e41a1daf9ba79f39b5022eb
Jorge Chavez <jorgechavez77@gmail.com>
2025-05-25 22:13:41 -0400

    Remove values

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
 
+const ASSET_PREFIX = process.env.ASSET_PREFIX;
+
 const nextConfig = (phase: string) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
   const nextConfig: NextConfig = {
     output: "standalone",
-    assetPrefix: isDev
-      ? undefined
-      : "https://dev-sls-nextjs-app-static.s3.us-east-1.amazonaws.com",
+    assetPrefix: isDev ? undefined : ASSET_PREFIX,
   };
   return nextConfig;
 };
