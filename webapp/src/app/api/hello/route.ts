3b2e05fa0f06fbdea125515d88bea8a9e5fa9f23
Jorge Chavez <jorgechavez77@gmail.com>
2025-05-25 21:57:45 -0400

    Improve api

--- webapp/src/app/api/hello/route.ts
+++ webapp/src/app/api/hello/route.ts
@@ -1,7 +1,5 @@
 export async function GET(request: Request) {
   console.log({ request });
 
-  const ENV_VARS = process.env;
-
-  return Response.json({ hi: ENV_VARS });
+  return Response.json({ hi: "Hello World!" });
 }
